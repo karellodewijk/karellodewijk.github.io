3f5b0a2e804f76da1a8c783eb7ea7264a9f82b32
Karel-Lodewijk Verdonck <pkalith@gmail.com>
2016-05-21 14:19:04 +0200

    update js

--- javascripts/lib/TransparencyHitArea.js
+++ javascripts/lib/TransparencyHitArea.js
@@ -410,7 +410,7 @@ PIXI.WebGLTransparencyHitArea.prototype.isTextureTransparentAt = function(x, y) 
     var index = (x + y * this.textureWidth) * 4 + 3;
 
     // value 255 means fully opaque, < 255 means (at least partially) transparent
-    return this.textureData[index] < 1;
+    return this.textureData[index] < 255;
 };
 
 /**
