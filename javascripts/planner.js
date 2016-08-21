9de89c2c12d45caa2a122d9fb6221e6ae15d063b
Karel-Lodewijk Verdonck <pkalith@gmail.com>
2016-08-21 21:44:18 +0200

    update

--- javascripts/planner.js
+++ javascripts/planner.js
@@ -3811,7 +3811,7 @@ function create_icon_cont(icon, texture) {
 	center_anchor(sprite);
 	
 	if (icon.label && icon.label != "") {
-		var text = create_text_sprite(icon.label, icon.label_color, ICON_LABEL_SCALE * icon.label_font_size, icon.label_font, icon.label_background, true, icon.label_font_modifier)
+		var text = create_text_sprite(icon.label, icon.label_color, ICON_LABEL_SCALE * icon.label_font_size, icon.label_font, icon.label_background, !icon.label_background, icon.label_font_modifier)
 	
 		var label_pos = icon.label_pos;
 		if (!label_pos) {
