e2313e3a9e21dce131eab4dc78881f83c3e7276d
Karel-Lodewijk Verdonck <pkalith@gmail.com>
2016-09-30 22:59:45 +0200

    update

---
+++ canvas-webgl/test/canvas_bug.js
@@ -0,0 +1,35 @@
+function test_webgl(test) {
+	var canvas = document.createElement('canvas');
+	canvas.setAttribute("style", "border-style: solid; border-width: 5px; margin:5px");
+	canvas.width = 300;
+	canvas.height = 300;
+	ctx = canvas.getContext('webgl-2d');
+	document.body.appendChild(canvas);
+	test(ctx);
+}
+
+function test_canvas(test) {
+	var canvas = document.createElement('canvas');
+	canvas.setAttribute("style", "border-style: solid; border-width: 5px; margin:5px");
+	canvas.width = 300;
+	canvas.height = 300;
+	ctx = canvas.getContext('2d');
+	document.body.appendChild(canvas);
+	test(ctx);
+}
+
+function do_test(test) {
+	test_canvas(test);
+	test_webgl(test);
+	document.body.appendChild(document.createElement('br'));
+}
+
+window.onload = function() {
+	do_test(function(ctx) {
+		ctx.font = "30px Arial";
+		ctx.shadowColor = "black";
+		ctx.shadowOffsetX = 5; 
+		ctx.shadowOffsetY = 5; 
+		ctx.fillText("Hello world", 50, 50);
+	});
+}
