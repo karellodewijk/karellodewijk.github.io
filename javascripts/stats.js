833483b249ebe345c0cf3c615cdb9ef01f1ae19c
Karel-Lodewijk Verdonck <pkalith@gmail.com>
2017-01-22 04:35:01 +0100

    update

--- javascripts/stats.js
+++ javascripts/stats.js
@@ -149,7 +149,7 @@ function populate() {
 		}
 				
 		$( document ).ready(function() {
-			$("#tank_list").tablesorter({sortList: [[5,1], [0,0],[1,0],[2,0],[3,0],[4,0],[6,0],[7,0]], sortStable:true, sortAppend: [[5,1]]}); 		
+			$("#tank_list").tablesorter({sortList: [[5,1], [0,0],[1,0],[2,0],[3,0],[4,0],[6,0],[7,0],[8,0],[9,0],[10,0],[11,0],[12,0],[13,0]], sortStable:true, sortAppend: [[5,1]]}); 		
 			
 			function filter_tank_data(tier) {
 				for (var i in stats_data) {
